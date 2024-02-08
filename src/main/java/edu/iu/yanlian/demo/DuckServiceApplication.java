ec095fe2e70c2db47366d8e6ef85048b7c12412c
yanlian <yanlian@iu.edu>
2024-02-07 22:40:21 -0500

    Initial commit

---
+++ src/main/java/edu/iu/yanlian/demo/DuckServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.yanlian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/yanlian/demo/DuckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.yanlian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
